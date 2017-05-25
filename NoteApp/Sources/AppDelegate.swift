e116396495643e255d9f2aea406655ec6349b0e7
seoju <wngus606@gmail.com>
2017-05-25 17:05:51 +0900

    파일 경로 수정

---
+++ NoteApp/Sources/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  NoteApp
+//
+//  Created by seo on 2017. 5. 20..
+//  Copyright © 2017년 seoju. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        let window: UIWindow = UIWindow(frame: UIScreen.main.bounds)
+        window.backgroundColor = .white
+        window.makeKeyAndVisible()
+        
+        let mainViewController: MainViewController = MainViewController()
+        let navicationViewController: UINavigationController = UINavigationController(rootViewController: mainViewController)
+        window.rootViewController = navicationViewController
+        self.window = window
+        
+        return true
+    }
+}
+
--- NoteApp/Sources/ViewControllers/MainViewController.swift
+++ NoteApp/Sources/ViewControllers/MainViewController.swift
@@ -1,25 +1,23 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  NoteApp
 //
-//  Created by seo on 2017. 5. 20..
+//  Created by seo on 2017. 5. 25..
 //  Copyright © 2017년 seoju. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
